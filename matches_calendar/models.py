d23e4dc2f6c81881b1163db88d050c59ced7bf10
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-14 12:06:16 +0200

    Update models.py

--- matches_calendar/models.py
+++ matches_calendar/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
