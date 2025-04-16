5199dd720dc207fae9c1f461507e7081b804d942
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 14:50:14 +0200

    Create Admin

--- matches_calendar/admin.py
+++ matches_calendar/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Match, Team
 
-# Register your models here.
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('matchday', 'date', 'home_team', 'away_team', 'score_home', 'score_away', 'competition', 'season', 'is_cancelled')
+    list_filter = ('competition', 'is_cancelled', 'home_team', 'away_team')
+    search_fields = ('matchday', 'home_team__name', 'away_team__name', 'competition', 'season')
+    ordering = ('-date',)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
