2c88a7e22095ffc8c146331ec6e43f57215604a9
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 12:52:48 +0200

    Update urls.py

--- matches_calendar/urls.py
+++ matches_calendar/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import MatchListView, MatchDetailView, MatchListFromLocalFile
+from .views import LeagueListView, MatchListView, MatchDetailView, LeagueDetailView, MatchListFromLocalFile
 
 urlpatterns = [
+    path('leagues/', LeagueListView.as_view(), name='league-list'),
+    path('leagues/<int:pk>/', LeagueDetailView.as_view(), name='league-detail'),
     path('matches/', MatchListView.as_view(), name='match-list'),
     path('matches/<int:pk>/', MatchDetailView.as_view(), name='match-detail'),
     path('matches-from-local/', MatchListFromLocalFile.as_view(), name='match-list-local'),
