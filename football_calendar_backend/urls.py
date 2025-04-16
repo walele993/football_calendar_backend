b11835453b4a09cb4c88a724f6d2a998b516099b
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 19:10:10 +0200

    Starting Deployment

--- football_calendar_backend/urls.py
+++ football_calendar_backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Football Calendar API is running"})
 
 urlpatterns = [
+    path('', home),  # <- aggiunto
     path('admin/', admin.site.urls),
-    path('api/', include('matches_calendar.urls')), 
+    path('api/', include('matches_calendar.urls')),
 ]
