0c96c3783a5647ac626d7f16a10491ff593858a6
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 18:56:04 +0200

    Starting Deployment

--- manage.py
+++ manage.py
@@ -7,14 +7,16 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_calendar_backend.settings')
     try:
-        from football_calendar_backend.wsgi import application  # Importa application da wsgi.py
+        # Importa l'application dal file wsgi.py
+        from football_calendar_backend.wsgi import application
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    application  # Usa la variabile application per Vercel
+
+    application  
 
 if __name__ == '__main__':
     main()
