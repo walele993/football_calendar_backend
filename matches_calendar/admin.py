c25b72a9ab54249a29d83a7b84899dd4d9f93717
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 15:01:18 +0200

    Competition to League

--- matches_calendar/admin.py
+++ matches_calendar/admin.py
@@ -4,9 +4,10 @@ from .models import Match, Team, League
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('matchday', 'date', 'home_team', 'away_team', 'score_home', 'score_away', 'league', 'season', 'is_cancelled')
-    list_filter = ('league', 'is_cancelled', 'home_team', 'away_team')
-    search_fields = ('matchday', 'home_team__name', 'away_team__name', 'league__name', 'season')
+    list_filter = ('league', 'is_cancelled', 'home_team', 'away_team') 
+    search_fields = ('matchday', 'home_team__name', 'away_team__name', 'league__name', 'season')  
     ordering = ('-date',)
+
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
