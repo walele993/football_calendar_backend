95ca0247a7063b708fdeecdc15400e3cc4e22025
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-16 22:09:07 +0200

    Backup and Restore

--- football_calendar_backend/settings.py
+++ football_calendar_backend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = [
+    '.vercel.app',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
