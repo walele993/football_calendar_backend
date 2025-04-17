fb0d0f12ca6cd098505bb939bbc6d4505d6c65a5
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-17 12:37:58 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,8 +7,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_calendar_backend.settings')
     try:
-        # Importa l'application dal file wsgi.py
-        from football_calendar_backend.wsgi import application
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
