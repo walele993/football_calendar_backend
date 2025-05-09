5fbf7776775f28f12a8b9449949bb807e3da2987
walele993 <64219379+walele993@users.noreply.github.com>
2025-05-09 10:39:29 +0200

    Update commands

--- matches_calendar/management/commands/update_matches.py
+++ matches_calendar/management/commands/update_matches.py
@@ -1,19 +1,16 @@
-# matches_calendar/management/commands/update_matches.py
 import os
+from utils import update_matches_from_remote_repo
 
-from django.core.management.base import BaseCommand
-from matches_calendar.utils import update_matches_from_remote_repo
+def main():
+    repo_url = "https://github.com/walele993/football_calendar_project.git"
+    mongo_uri = os.getenv("MONGO_URI")
 
-class Command(BaseCommand):
-    help = "Update matches from the remote parsed_json folder in the football_calendar_project repository"
+    if not mongo_uri:
+        print("Error: MONGO_URI environment variable is not set.")
+        return
 
-    def handle(self, *args, **options):
-        repo_url = "https://github.com/walele993/football_calendar_project.git"
-        mongo_uri = os.getenv("MONGO_URI")
+    message = update_matches_from_remote_repo(repo_url=repo_url, mongo_uri=mongo_uri)
+    print(message)
 
-        if not mongo_uri:
-            self.stderr.write(self.style.ERROR("MONGO_URI environment variable is not set."))
-            return
-
-        message = update_matches_from_remote_repo(repo_url=repo_url, mongo_uri=mongo_uri)
-        self.stdout.write(self.style.SUCCESS(message))
+if __name__ == "__main__":
+    main()
